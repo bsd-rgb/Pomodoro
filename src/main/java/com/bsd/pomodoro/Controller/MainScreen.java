7945d803bf096750c8c5a45039350b400d03f874
Brandi <brandi.davis2483@gmail.com>
2024-11-10 21:06:21 -0600

    Created activity states. Updated timer to display break timer. Settings gear added with click event.

--- src/main/java/com/bsd/pomodoro/Controller/MainScreen.java
+++ src/main/java/com/bsd/pomodoro/Controller/MainScreen.java
@@ -1,5 +1,6 @@
 package com.bsd.pomodoro.Controller;
 
+import com.bsd.pomodoro.Model.ActivityState;
 import javafx.animation.Animation;
 import javafx.animation.PauseTransition;
 import javafx.beans.binding.Bindings;
@@ -9,8 +10,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Duration;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,15 +29,43 @@ public class MainScreen implements Initializable {
     private Button startButton;
     @FXML
     private Label lblTimer;
-    private PauseTransition timer = new PauseTransition(Duration.minutes(1));
+    @FXML
+    private Label lblActivityState;
+    private PauseTransition focusTimer = new PauseTransition(Duration.seconds(10));
+    private PauseTransition breakTimer = new PauseTransition(Duration.seconds(5));
+    private ActivityState state;
+    @FXML
+    private ImageView settingImage;
+
+
+//image view is a node used for painting images loaded with images
+    //image = photograph
+    //imageView = picture frame
+
+    Image img  = new Image("/settingsIcon.png");
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        lblTimer.textProperty().bind(timeLeft(timer));
-        timer.setOnFinished(e ->{
-            System.out.println("Timer finished");
-            timer.stop();
-            timer.jumpTo(Duration.ZERO);
+
+        state = ActivityState.valueOf("FOCUS");
+        lblTimer.textProperty().bind(timeLeft(focusTimer));
+
+
+        focusTimer.setOnFinished(e ->{
+            System.out.println("Focus Timer finished");
+            focusTimer.stop();
+            breakTimer.jumpTo(Duration.ZERO);
+            state = ActivityState.valueOf("BREAK");
+            lblTimer.textProperty().bind(timeLeft(breakTimer));
+            lblActivityState.setText(state.toString());
+        });
+        breakTimer.setOnFinished(e ->{
+            System.out.println("Break Timer finished.");
+            breakTimer.stop();
+            focusTimer.jumpTo(Duration.ZERO);
+            state = ActivityState.valueOf("FOCUS");
+            lblActivityState.setText(state.toString());
         });
 
     }
@@ -37,16 +73,32 @@ public class MainScreen implements Initializable {
 
     @FXML
     void OnActionStart(ActionEvent event) {
-        timer.play();
-        System.out.println("Timer Started.");
+        if(state.toString().equalsIgnoreCase("focus")){
+            focusTimer.play();
+            settingImage.setImage(img);
+            System.out.println("focus Timer Started.");
+            state = ActivityState.valueOf("FOCUS");
+            lblActivityState.setText(state.toString());
+
+        }
+        if(state.toString().equalsIgnoreCase("break")){
+            breakTimer.play();
+            System.out.println("break Timer Started.");
+            state = ActivityState.valueOf("BREAK");
+            lblTimer.textProperty().bind(timeLeft(breakTimer));
+            lblActivityState.setText(state.toString());
+
+        }
+
+
     }
 
     public void onActionPause(ActionEvent actionEvent) {
-        timer.pause();
+        focusTimer.pause();
         System.out.println("Timer Paused.");
     }
     public void onActionStop(ActionEvent actionEvent) {
-        timer.stop();
+        focusTimer.stop();
         System.out.println("Timer Stopped.");
     }
 
@@ -61,5 +113,10 @@ public class MainScreen implements Initializable {
                 },
                 animation.currentTimeProperty(),
                 animation.cycleDurationProperty());
+    }
+
+
+    public void mousePressed() {
+        System.out.println("Settings gear clicked.");
     }
 }
---
+++ src/main/java/com/bsd/pomodoro/Model/ActivityState.java
@@ -0,0 +1,6 @@
+package com.bsd.pomodoro.Model;
+
+public enum ActivityState {
+
+    FOCUS, BREAK;
+}
